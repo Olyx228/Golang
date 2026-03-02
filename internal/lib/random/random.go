cd360443846b9de21b523aafc37b6979fe692b75
Семён Нефедов <semennefiodov@yandex.ru>
2026-03-02 17:35:59 +0700

    new random, test random succes

--- internal/lib/random/random.go
+++ internal/lib/random/random.go
@@ -1,21 +1,25 @@
 package random
 
 import (
-	"math/rand"
-	"time"
+	"crypto/rand"
+	"math/big"
 )
 
-// NewRandomString generates random string with given size.
+// NewRandomString generates random string with given size using crypto/rand
 func NewRandomString(size int) string {
-	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
-
 	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
 		"abcdefghijklmnopqrstuvwxyz" +
 		"0123456789")
 
 	b := make([]rune, size)
 	for i := range b {
-		b[i] = chars[rnd.Intn(len(chars))]
+		// Генерируем криптостойкое случайное число
+		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(chars))))
+		if err != nil {
+			// В случае ошибки используем fallback (но на практике её не будет)
+			panic(err)
+		}
+		b[i] = chars[n.Int64()]
 	}
 
 	return string(b)
